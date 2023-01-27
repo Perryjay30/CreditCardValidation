c5ddc035a8c7dffa48c0a66f8f9ad1fa009f86e7
Perryjay30 <pelumijsh@gmail.com>
2023-01-27 06:03:56 -1200

    CreditCardMain

---
+++ src/CreditCardValidation/Main.java
@@ -0,0 +1,32 @@
+package CreditCardValidation;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        System.out.print("""
+                CREDIT CARD VALIDATION!!!
+                Press 1 to enter the credit card number you want to validate
+                Press 2 to exit""");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter an option: ");
+        int userInput = sc.nextInt();
+        switch(userInput) {
+            case 1 -> checkValidation();
+            case 2 -> exit(2);
+        }
+    }
+
+    private static void checkValidation() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your credit number to check validation");
+        String cardNumber = input.nextLine();
+        if (CreditCard.isValidCreditCard(cardNumber)) System.out.println("Is valid");
+        else
+            throw new InvalidCreditCardException("Credit card can't be validated");
+     }
+
+}
